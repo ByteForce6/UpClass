483a05d68be8c80d07ac0787b4c22de5630b6df9
Anthony <cristo045millanperez@gmail.com>
2026-05-14 23:26:54 -0600

    manejo de rutas

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import UpClassDashboard from './UpClassDashboard.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 import './App.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <UpClassDashboard />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 )
