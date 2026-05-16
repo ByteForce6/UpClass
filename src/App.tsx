4db5bcd5c4a2cf6e2788e22f72c1298e5aa8cf6d
Anthony <cristo045millanperez@gmail.com>
2026-05-15 21:02:07 -0600

    Vista del Alumno

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import UpClassDashboard from "./pages/UpClassDashboard";
+import Calificaciones from "./pages/students/Calificaciones";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<UpClassDashboard />} />
+      <Route path="/students" element={<Calificaciones />} />
     </Routes>
   );
 }
