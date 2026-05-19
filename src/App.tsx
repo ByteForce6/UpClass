c34ea1f4502f3315107892a87fdff80210f0db71
Anthony <cristo045millanperez@gmail.com>
2026-05-18 21:36:59 -0600

    App.tsx corregido

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import UpClassDashboard from "./views/administrator/UpClassDashboard";
-import CursosView from "./views/administrator/CursosView";
-import InstructoresView from "./views/administrator/InstructoresView";
-import AlumnosView from "./views/administrator/AlumnosView";
-import Calificaciones from "./views/students/Calificaciones";
-import CatalogoCursos from "./views/students/CatalogoCursos";
+import UpClassDashAlunmo from "./views/students/UpClassDashAlunmo";
+
 import Home from "./views/home";
 import Login from "./views/login";
-
+import NotFound from "./views/NotFound";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<UpClassDashboard />} />
-      <Route path="/cursos" element={<CursosView />} />
-      <Route path="/instructores" element={<InstructoresView />} />
-      <Route path="/alumnos" element={<AlumnosView />} />
-      <Route path="/students" element={<Calificaciones />} />
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Home />} />
+
+      <Route path="/admin" element={<UpClassDashboard />} />
+      <Route path="/students" element={<UpClassDashAlunmo />} />
+
       <Route path="/login" element={<Login />} />
 
-      <Route path="/xd" element={<CatalogoCursos />} />
 
-      <Route path="*" element={<CatalogoCursos />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
