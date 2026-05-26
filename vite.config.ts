3419218503d2ccea19d7765bdae3b00ebaae454a
Anthony <cristo045millanperez@gmail.com>
2026-05-25 22:44:14 -0600

    Correciones de scrool y algunos maskable

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico'],
       manifest: {
+        id: '/',
         name: 'UpClass',
         short_name: 'UpClass',
         description: 'Plataforma educativa para cursos en línea',
@@ -17,25 +18,121 @@ export default defineConfig({
         display: 'standalone',
         orientation: 'portrait',
         start_url: '/',
+        screenshots: [
+          {
+            src: '/screenshot_horizontal.png',
+            sizes: '2704x1320',
+            type: 'image/png',
+            form_factor: 'wide',
+            label: 'UpClass'
+          },
+          {
+            src: '/screenshot_vertical.png',
+            sizes: '926x1334',
+            type: 'image/png',
+            form_factor: 'narrow',
+            label: 'UpClass (2)'
+          }
+        ],
         icons: [
           {
-            src: '/pwa-192x192.png',
+            src: '/maskable_icon_x48.png',
+            sizes: '48x48',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/maskable_icon_x72.png',
+            sizes: '72x72',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/maskable_icon_x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/maskable_icon_x128.png',
+            sizes: '128x128',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/maskable_icon_x192.png',
             sizes: '192x192',
             type: 'image/png',
+            purpose: 'any'
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/maskable_icon_x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/maskable_icon_x512.png',
             sizes: '512x512',
             type: 'image/png',
+            purpose: 'any'
           },
           {
-            src: '/pupwa-512x512.png',
-            sizes: '512x291',
+            src: '/maskable_icon.png',
+            sizes: '2000x2000',
             type: 'image/png',
-            purpose: 'maskable',
+            purpose: 'any'
           },
-        ],
-      },
+          {
+            src: '/maskable_icon_x48.png',
+            sizes: '48x48',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/maskable_icon_x72.png',
+            sizes: '72x72',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/maskable_icon_x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/maskable_icon_x128.png',
+            sizes: '128x128',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/maskable_icon_x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/maskable_icon_x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/maskable_icon_x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/maskable_icon.png',
+            sizes: '2000x2000',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
     }),
   ],
 })
